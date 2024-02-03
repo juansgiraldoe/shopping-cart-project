f0e507c23a1ec84c9da015ed52c6b4c94f917b31
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-03 11:33:19 -0500

    [Update] - Add click event on the button on each side of the course list.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const carrito =  document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+  //Agregar curso al acrrito.
+  listaCursos.addEventListener('click', agregarCurso)
+};
+
+//Funciones:
+function agregarCurso(e) {
+  e.preventDefault();
+  if ( e.target.classList.contains('agregar-carrito') ) {
+    const cursoSeleccionado = e.target.parentElement.parentElement//.children[1].children[0].textContent;
+    readDocument(cursoSeleccionado);
+  }
+}
