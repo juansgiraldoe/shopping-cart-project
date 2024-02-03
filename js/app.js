e76a291ff222a84102fe69f2d68b0c2a76679ec3
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-03 12:28:30 -0500

    [Update] - Show selected items in shopping cart.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const carrito =  document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito =[]
 
 cargarEventListeners();
 function cargarEventListeners() {
-  //Agregar curso al acrrito.
+  //Agregar curso al carrito.
   listaCursos.addEventListener('click', agregarCurso)
 };
 
@@ -29,5 +30,39 @@ function readDocument(curso) {
     id: curso.querySelector('a').getAttribute('data-id'),
     cantidad: 1,
   }
-  console.log(infoCurso);
+
+  //Agrega elementos al arreglo del carrito.
+  articulosCarrito =[...articulosCarrito, infoCurso]
+  console.log(articulosCarrito);
+  carritoHTML();
+}
+
+//Muestra los elementos en el HTML del carrito de compras.
+function carritoHTML() {
+  //Limpia el html.
+  limpiarHmtl();
+
+  //Recorre el carrito y genera el HTML.
+  articulosCarrito.forEach( item => {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+    <td></td>
+    <td>
+      ${item.titulo}
+    </td>
+    `;
+
+    //Agrega el hml al tbody
+    contenedorCarrito.appendChild(row)
+  });
+
+  function limpiarHmtl() {
+    //Forma lenta.
+    //contenedorCarrito.innerHTML = '';
+    //Mejor performance.
+    while ( contenedorCarrito.firstChild ) {
+      contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
+  }
 }
